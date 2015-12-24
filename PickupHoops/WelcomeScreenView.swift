c319601b1ee793155a0f80b18ecaff5255c95a0d
Ahmed Musse <amusse@princeton.edu>
2015-12-23 19:32:58 -0600

    Added welcome screen with official logo and "login" and "signup" buttons. Also removed segue from welcome screen to tab bar controller, thus the compiling warning.

---
+++ PickupHoops/WelcomeScreenView.swift
@@ -0,0 +1,29 @@
+//
+//  WelcomeScreenView.swift
+//  PickupHoops
+//
+//  Created by Ahmed Musse on 12/23/15.
+//  Copyright © 2015 Pickup Sports. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+
+class WelcomeScreenView: UIViewController {
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
