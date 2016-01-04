524788d1ae07f9c2e5110af49697d4cabcd1c9e1
Ahmed Musse <amusse@princeton.edu>
2016-01-04 12:25:53 -0500

    Created new game object and saved to Parse database.

--- PickupHoops/NewGameVC.swift
+++ PickupHoops/NewGameVC.swift
@@ -18,13 +18,24 @@ class NewGameVC: UITableViewController
     @IBOutlet weak var pckrDate: UIDatePicker!
     @IBOutlet weak var btnPickerDone: UIButton!
     @IBOutlet weak var lLocation: UILabel!
+    @IBOutlet weak var scType: UISegmentedControl!
+    @IBOutlet weak var txtRating: UITextField!
     
     var row: Int = 0
     var selectedLocation = CLLocationCoordinate2D(latitude: 0, longitude: 0)
-    
+    var currentUser: PFUser!
+    var latitude = ""
+    var longitude = ""
+    var startTime = ""
+    var endTime = ""
+    var rating = 0
+    var notes = ""
+
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        currentUser = PFUser.currentUser()
+        
         tvNotes.text = "Notes"
         pckrDate.hidden = true
         btnPickerDone.hidden = true
@@ -39,6 +50,26 @@ class NewGameVC: UITableViewController
     @IBAction func btnDone(sender: AnyObject)
     {
         // Create new game object
+        let username = currentUser.username
+        let players = NSMutableArray()
+        players.addObject(username!)
+        
+        notes = tvNotes.text!
+        rating = Int(txtRating.text!)!
+        
+        
+        let game = PFObject(className: "Game")
+        game["type"]        = scType.titleForSegmentAtIndex(scType.selectedSegmentIndex)
+        game["location"]    = PFGeoPoint(latitude: Double(latitude)!, longitude: Double(longitude)!)
+        game["start"]       = startTime
+        game["end"]         = endTime
+        game["min_rating"]  = rating
+        game["notes"]       = notes
+        game["num_players"] = 1
+        game["players"]     = players
+        game["host"]        = username
+        
+        game.saveInBackground()
         self.dismissViewControllerAnimated(true, completion: nil)
     }
     
@@ -63,10 +94,12 @@ class NewGameVC: UITableViewController
         if (row == 2)
         {
            lStartTime.text = strDate
+            startTime = strDate
         }
         else if(row == 3)
         {
             lEndTime.text = strDate
+            endTime = strDate
         }
     }
 
@@ -80,7 +113,11 @@ class NewGameVC: UITableViewController
     {
         let mapVC: MapVC = unwindSegue.sourceViewController as! MapVC
         selectedLocation = mapVC.selectedLocation
-        lLocation.text = "coordinate: \(selectedLocation.latitude), \(selectedLocation.longitude)"
+        latitude = selectedLocation.latitude.description
+        longitude = selectedLocation.longitude.description
+        
+        lLocation.text = "coordinate: " + latitude + ", " + longitude
+        
     }
     
     
